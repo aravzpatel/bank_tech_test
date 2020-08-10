6d869e1de325f0cd4fa7d2aabf2f32d188035086
Arav <arav.patel@gmail.com>
2020-08-10 14:27:34 +0100

    Refactor deposit to extract input check

--- src/BankAccount.js
+++ src/BankAccount.js
@@ -4,13 +4,17 @@ class BankAccount{
   }
 
   deposit(amount){
-    if ( amount < 0) {
-      throw new Error("Deposit Issue: Negative amounts are not possible")
-    } else if (isNaN(amount)){
-      throw new Error("Deposit Issue: Invalid input, please provide a number")
-    }
+    this.isValidInput(amount)
     
     var incomingDeposit = { type: "Deposit", amount: amount, date: Date.now };
     this.transactionLog.push(incomingDeposit);
   };
+
+  isValidInput(input){
+    if ( input < 0) {
+      throw new Error("Deposit Issue: Negative amounts are not possible")
+    } else if (isNaN(input)){
+      throw new Error("Deposit Issue: Invalid input, please provide a number")
+    }
+  }
 };
